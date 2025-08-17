2ee8ed75e8c22d262783008c4af1978020610b4b
agarkovmax <agarkov444@gmail.com>
2025-08-17 14:43:53 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   site: 'https://greenzonesite.ru',
-  output: 'server',
-  adapter: vercel(),
+  output: 'static', // ВАЖНО: не 'server', а 'static'
   integrations: [tailwind()],
 });
